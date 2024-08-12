044324534662bc70674f27fd829929121fbf914c
Ann <ann20021007@gmail.com>
2024-08-12 10:16:20 +0100

    adjust hone endpoint

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ from app.main import app
 
 @app.route('/')
 def home():
-    return 'Flask API started on Render.'
+    return 'Flask API started on Pythonanywhere.'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True) # `127.0.0.1` only allowed local pc can visit
