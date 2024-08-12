b2c6f158672fd4105c392b0cc00418a75a96e6c7
Ann <ann20021007@gmail.com>
2024-08-12 10:00:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# flask-pythonanywhere-ml-cpu-ver1-cpu
---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from app.main import app
+# let app be base package
+
+@app.route('/')
+def home():
+    return 'Flask API started on Render.'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True) # `127.0.0.1` only allowed local pc can visit
